791aa7c148886333b331840f6c8700e931d11f70
Cameron Weston <cameronbweston@gmail.com>
2021-08-06 10:11:37 -0500

    Finish giphy alb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  state = {
+    giphyAPIKey: '&api_key=Nn819Vz4PODYs8ZzEXxCjmXaJW9TXH01',
+    limit: '',
+    searchGif: '',
+    searchURL: '',
+    returnedGifs: []
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  getGif = (event) => {
+    event.preventDefault()
+    try{
+        fetch(`http://api.giphy.com/v1/gifs/search?${this.state.giphyAPIKey}&q=${this.state.searchGif}&limit=1`)
+        .then(response => {
+          return response.json()
+        })
+        .then(json => {
+          this.setState({
+            returnedGifs: json.data,
+          })
+        })
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <h1>Giphy Lab</h1>
+          <form onSubmit={this.getGif}>
+        <input type="text" name="searchGif" value={this.state.searchGif} onChange={this.handleChange}/>
+        <button>Submit</button>
+      </form>
+           {this.state.returnedGifs.map((gif, idx) => {
+             return(
+               <img src={gif.images.original.url} key={idx} alt='GIF'/>
+             )
+           })}
+      </div>
+    );
+  }
 }
 
 export default App;
